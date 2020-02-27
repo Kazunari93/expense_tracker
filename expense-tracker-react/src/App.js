efe172d406135fce665162521b9d1aa58f2c51f0
Kazunari93 <aooni0930@gmail.com>
2020-02-27 23:13:55 +0900

    AddTransaction-component&modified

--- expense-tracker-react/src/App.js
+++ expense-tracker-react/src/App.js
@@ -3,6 +3,7 @@ import { Header } from "./components/Header";
 import { Balance } from "./components/Balance";
 import { IncomeExpenses } from "./components/IncomeExpenses";
 import { Transaction } from "./components/Transaction";
+import { AddTransaction } from "./components/AddTransaction";
 
 import "./App.css";
 
@@ -10,10 +11,11 @@ function App() {
   return (
     <div>
       <Header />
-      <div class="container">
+      <div className="container">
         <Balance />
         <IncomeExpenses />
         <Transaction />
+        <AddTransaction />
       </div>
     </div>
   );
--- expense-tracker-react/src/components/IncomeExpenses.jsx
+++ expense-tracker-react/src/components/IncomeExpenses.jsx
@@ -2,18 +2,14 @@ import React from "react";
 
 export const IncomeExpenses = () => {
   return (
-    <div class="inc-exp-container">
+    <div className="inc-exp-container">
       <div>
         <h4>収入</h4>
-        <p id="money-plus" class="money plus">
-          0.00円
-        </p>
+        <p className="money plus">0.00円</p>
       </div>
       <div>
         <h4>支出</h4>
-        <p id="money-minus" class="money minus">
-          -0.00円
-        </p>
+        <p className="money minus">-0.00円</p>
       </div>
     </div>
   );
