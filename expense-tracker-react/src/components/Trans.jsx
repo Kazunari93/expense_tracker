42469c3a683fe33a30569ae4c730b75779020f10
Kazunari93 <aooni0930@gmail.com>
2020-02-28 22:39:36 +0900

    modified

--- expense-tracker-react/src/components/Trans.jsx
+++ expense-tracker-react/src/components/Trans.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 
 export const Trans = ({ transaction }) => {
+  const sign = transaction.amount < 0 ? "-" : "+";
   return (
     <div>
-      <li className="minus">
-        {transaction.text} <span>-40000円</span>
+      <li className={transaction.amount < 0 ? "minus" : "plus"}>
+        {transaction.text}{" "}
+        <span>
+          {sign}
+          {Math.abs(transaction.amount)}円
+        </span>
         <button className="delete-btn">x</button>
       </li>
     </div>
