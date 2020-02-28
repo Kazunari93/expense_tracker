56897955c6f8a9ee5a048696e8fc6ee67b291b9a
Kazunari93 <aooni0930@gmail.com>
2020-02-28 20:36:11 +0900

    GlobalState

---
+++ expense-tracker-react/src/context/GlobalState.jsx
@@ -0,0 +1,23 @@
+import React, { createContext, useReducer } from "react";
+import AppReducer from "./AppReducer";
+
+const initialState = {
+  transaction: [
+    { id: 1, text: "花", amount: -20 },
+    { id: 2, text: "給料", amount: 300 },
+    { id: 3, text: "本", amount: -10 },
+    { id: 4, text: "カメラ", amount: 150 }
+  ]
+};
+
+export const GlobalContext = createContext(initialState);
+
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  return (
+    <GlobalContext.Provider value={{ transaction: state.transaction }}>
+      {children}
+    </GlobalContext.Provider>
+  );
+};
